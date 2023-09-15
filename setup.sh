d631f2977ca5adf2445d33e480b514165350d2e7
rummsi1337 <8877468+rummsi1337@users.noreply.github.com>
2023-09-15 08:29:10 +0200

    Update setup.sh script
    - Update download url of docker-compose.yml
    - Rename install path variable

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
 DOCKER_VOLUME="/volume1/docker"
-PROMETHEUS_ROOT="${DOCKER_VOLUME}/prometheus"
+NODE_EXPORTER_ROOT="${DOCKER_VOLUME}/node-exporter"
 
-[ ! -d ${PROMETHEUS_ROOT} ] && mkdir -p ${PROMETHEUS_ROOT} \
-&& echo "folder created ${PROMETHEUS_ROOT}"
+[ ! -d ${NODE_EXPORTER_ROOT} ] && mkdir -p ${NODE_EXPORTER_ROOT} \
+&& echo "folder created ${NODE_EXPORTER_ROOT}"
 
-curl -fsS -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/lucasmaurice/synology-prometheus/master/docker-compose.yml > ${PROMETHEUS_ROOT}/docker-compose.yml
-echo "docker-compose file created ${PROMETHEUS_ROOT}/docker-compose.yml"
+curl -fsS -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/rummsi1337/synology-prometheus-node-exporter/master/docker-compose.yml > ${NODE_EXPORTER_ROOT}/docker-compose.yml
+echo "docker-compose file created ${NODE_EXPORTER_ROOT}/docker-compose.yml"
 
-docker-compose -f ${PROMETHEUS_ROOT}/docker-compose.yml up -d
+docker-compose -f ${NODE_EXPORTER_ROOT}/docker-compose.yml up -d
 
 echo "
 #############################################
